1c6cd9465c9cbab0ba1a1d22901e0497779ef6a5
Luiz0tavio <me@luizotav.io>
2018-09-19 13:45:16 -0300

    point folder

---
+++ Assets/Point/PointCloud.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+  
+  
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class PointCloud : MonoBehaviour {
+  
+	private Mesh mesh;
+	int numPoints = 2;
+  
+	void Start () 
+	{
+		mesh = new Mesh();
+  
+		GetComponent<MeshFilter>().mesh = mesh;
+		CreateMesh();
+	}
+  
+	void CreateMesh() 
+	{
+		Vector3[] points 	= new Vector3[numPoints];
+		int[] indices 		= new int[numPoints];
+		
+		points[0]  = new Vector3(0,0,0);
+		indices[0] = 0;
+  
+		points[1]  = new Vector3(0,0,0);
+		indices[1] = 1;
+		
+		mesh.vertices = points;
+		mesh.SetIndices(indices, MeshTopology.Points, 0);
+	}
+}
