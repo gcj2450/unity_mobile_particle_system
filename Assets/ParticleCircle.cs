ecf5c23bbaf59623b7ec8fe2e936c4ad6fc5c442
Luiz0tavio <me@luizotav.io>
2018-10-02 18:36:21 -0300

    single draw call

--- Assets/ParticleCircle.cs
+++ Assets/ParticleCircle.cs
@@ -14,90 +14,83 @@ public class ParticleCircle : MonoBehaviour
     public Position emitterPosition;
     public float particleSize           = 0.05f;
     public float particleSpeedScale     = 10.0f;
-    public int totalParticles           = 2000;
+    public int totalParticles           = 200;
     public int lifeTimeInSeconds        = 10;
     public bool respawn                 = false;
 
+    private GameObject particles;
+
     private float totalTime;
-    private  List<GameObject> particles;
-    
+
     public void Start()
     {
+        totalTime = 0f;
+
         Shader shader = Shader.Find("Unlit/ParticleCircle");
 
-        particles = new List<GameObject>();
-        totalTime = 0f;
-            
+        particles = GameObject.CreatePrimitive(PrimitiveType.Quad);
+        
+        Renderer renderer = particles.GetComponent<Renderer>();
+        renderer.material.shader = shader;
+        renderer.material.SetFloat("_ParticleSize", particleSize);
+        renderer.material.SetFloat("_ParticleSpeedScale", particleSpeedScale);
+
+        Vector3[] vertices = new Vector3[4*totalParticles];
+        int[]     tri      = new int[6*totalParticles];
+        Vector2[] uv       = new Vector2[4*totalParticles];
+        Vector2[] id_time  = new Vector2[4*totalParticles];
+
         for (int i = 0; i < totalParticles; i++)
         {
-            particles.Add(GameObject.CreatePrimitive(PrimitiveType.Quad));
-            
-            Renderer renderer = particles[i].GetComponent<Renderer>();
-            renderer.material.shader = shader;
-            renderer.material.SetFloat("_ParticleSize", particleSize);
-            renderer.material.SetFloat("_ParticleSpeedScale", particleSpeedScale);
-            
-            MeshFilter mf = particles[i].GetComponent<MeshFilter>();
-
-            var mesh = new Mesh();
-            mf.mesh = mesh;
+            vertices[i*4 + 0] = new Vector3(emitterPosition.x, emitterPosition.y, emitterPosition.z);
+            vertices[i*4 + 1] = new Vector3(emitterPosition.x, emitterPosition.y, emitterPosition.z);
+            vertices[i*4 + 2] = new Vector3(emitterPosition.x, emitterPosition.y, emitterPosition.z);
+            vertices[i*4 + 3] = new Vector3(emitterPosition.x, emitterPosition.y, emitterPosition.z);
     
-            Vector3[] vertices = new Vector3[4];
-            vertices[0] = new Vector3(emitterPosition.x, emitterPosition.y, emitterPosition.z);
-            vertices[1] = new Vector3(emitterPosition.x, emitterPosition.y, emitterPosition.z);
-            vertices[2] = new Vector3(emitterPosition.x, emitterPosition.y, emitterPosition.z);
-            vertices[3] = new Vector3(emitterPosition.x, emitterPosition.y, emitterPosition.z);
-            mesh.vertices = vertices;
+            tri[i*6 + 0] = i*4 + 0;
+            tri[i*6 + 1] = i*4 + 2;
+            tri[i*6 + 2] = i*4 + 1;
+            tri[i*6 + 3] = i*4 + 2;
+            tri[i*6 + 4] = i*4 + 3;
+            tri[i*6 + 5] = i*4 + 1;
     
-            int[] tri = new int[6];
-            tri[0] = 0;
-            tri[1] = 2;
-            tri[2] = 1;
-            tri[3] = 2;
-            tri[4] = 3;
-            tri[5] = 1;
-            mesh.triangles = tri;
-    
-            Vector2[] uv = new Vector2[4];
-            uv[0] = new Vector2(0, 0);
-            uv[1] = new Vector2(1, 0);
-            uv[2] = new Vector2(0, 1);
-            uv[3] = new Vector2(1, 1);
-            mesh.uv = uv;
-
-            float time = 0f;
+            uv[i*4 + 0] = new Vector2(0, 0);
+            uv[i*4 + 1] = new Vector2(1, 0);
+            uv[i*4 + 2] = new Vector2(0, 1);
+            uv[i*4 + 3] = new Vector2(1, 1);
             
-            // Passing ID and Time as UV2.
-            Vector2[] id_time = new Vector2[4];
-            id_time[0] = new Vector2(i+1, time);
-            id_time[1] = new Vector2(i+1, time);
-            id_time[2] = new Vector2(i+1, time);
-            id_time[3] = new Vector2(i+1, time);
-            mesh.uv2 = id_time;
+            // Passing ID and Initial Time as UV2.
+            id_time[i*4 + 0] = new Vector2(i+1, 0);
+            id_time[i*4 + 1] = new Vector2(i+1, 0);
+            id_time[i*4 + 2] = new Vector2(i+1, 0);
+            id_time[i*4 + 3] = new Vector2(i+1, 0);
         }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices   = vertices;
+        mesh.triangles  = tri;
+        mesh.uv         = uv;
+        mesh.uv2        = id_time;
+        
+        particles.GetComponent<MeshFilter>().mesh = mesh;
     }
 
     public void Update()
     {
         totalTime += Time.deltaTime;
+        MeshFilter mf = particles.GetComponent<MeshFilter>();
+
+        Vector2[] id_time = new Vector2[4*totalParticles];
         for(int i = 0; i < totalParticles; i++) {
-
-            MeshFilter mf = particles[i].GetComponent<MeshFilter>();
-            
-            Vector2[] id_time = new Vector2[4];
-            id_time[0] = new Vector2(mf.mesh.uv2[0].x, totalTime);
-            id_time[1] = new Vector2(mf.mesh.uv2[1].x, totalTime);
-            id_time[2] = new Vector2(mf.mesh.uv2[2].x, totalTime);
-            id_time[3] = new Vector2(mf.mesh.uv2[3].x, totalTime);
-
-            mf.mesh.uv2 = id_time;
+            id_time[i*4 + 0] = new Vector2(mf.mesh.uv2[i*4 + 0].x, totalTime);
+            id_time[i*4 + 1] = new Vector2(mf.mesh.uv2[i*4 + 1].x, totalTime);
+            id_time[i*4 + 2] = new Vector2(mf.mesh.uv2[i*4 + 2].x, totalTime);
+            id_time[i*4 + 3] = new Vector2(mf.mesh.uv2[i*4 + 3].x, totalTime);
         }
 
+        mf.mesh.uv2 = id_time;
+
         if (totalTime >= lifeTimeInSeconds) {
-            for (int i = 0; i < totalParticles; i++) 
-                Destroy(particles[i]);
-            particles.Clear();
-            
             if (respawn) Start();
         }
     }
