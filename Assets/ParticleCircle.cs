55801d17f825da01f2b489e3a8bcc866e2d31c0e
Luiz0tavio <me@luizotav.io>
2018-10-27 13:23:59 -0300

    multiple instances of Prefab (different material and mesh)

--- Assets/ParticleCircle.cs
+++ Assets/ParticleCircle.cs
@@ -47,6 +47,8 @@ public class ParticleCircle : MonoBehaviour
     }
 
     public Collision collision;
+
+    private Mesh mesh;
     
     static private int MAX_COLLISION_PLANES = 4;
     
@@ -88,9 +90,7 @@ public class ParticleCircle : MonoBehaviour
             id[idx4 + 3] = new Vector2(i+1, 0);
         }
 
-        Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
         mesh.Clear();
-        
         mesh.vertices  = vertices;
         mesh.triangles = tri;
         mesh.uv        = uv;
@@ -103,7 +103,15 @@ public class ParticleCircle : MonoBehaviour
     void Awake()
     {
         Renderer renderer = GetComponent<Renderer>();
-        renderer.sharedMaterial.shader = Shader.Find("Unlit/ParticleCircle");
+        renderer.material = new Material(Shader.Find("Unlit/ParticleCircle"));
+
+#if UNITY_EDITOR
+        MeshFilter mf = GetComponent<MeshFilter>();
+        Mesh meshCopy = Mesh.Instantiate(mf.sharedMesh) as Mesh;
+        mesh = mf.mesh = meshCopy;
+#else
+        mesh = GetComponent<MeshFilter>().mesh;
+#endif
 
         int max_p = (int)Mathf.Ceil(startLifetime*emission.rateOverTime);
         if (max_p > maxParticles){
@@ -121,33 +129,36 @@ public class ParticleCircle : MonoBehaviour
     {
 
 #if UNITY_EDITOR
+        
         Renderer renderer = GetComponent<Renderer>();
-        if (renderer.sharedMaterial.GetFloat("_StartSize") != startSize){
-            renderer.sharedMaterial.SetFloat("_StartSize", startSize);
+        var tempMaterial = new Material(Shader.Find("Unlit/ParticleCircle"));
+        
+        if (tempMaterial.GetFloat("_StartSize") != startSize){
+            tempMaterial.SetFloat("_StartSize", startSize);
         }
-        if (renderer.sharedMaterial.GetFloat("_RateOverTime") != emission.rateOverTime){
-            renderer.sharedMaterial.SetFloat("_RateOverTime", emission.rateOverTime);
+        if (tempMaterial.GetFloat("_RateOverTime") != emission.rateOverTime){
+            tempMaterial.SetFloat("_RateOverTime", emission.rateOverTime);
         }
-        if (renderer.sharedMaterial.GetFloat("_StartSpeed") != startSpeed){
-            renderer.sharedMaterial.SetFloat("_StartSpeed", startSpeed);
+        if (tempMaterial.GetFloat("_StartSpeed") != startSpeed){
+            tempMaterial.SetFloat("_StartSpeed", startSpeed);
         }
-        if (renderer.sharedMaterial.GetFloat("_StartLifeTime") != startLifetime){
-            renderer.sharedMaterial.SetFloat("_StartLifeTime", startLifetime);
+        if (tempMaterial.GetFloat("_StartLifeTime") != startLifetime){
+            tempMaterial.SetFloat("_StartLifeTime", startLifetime);
         }
-        if (renderer.sharedMaterial.GetFloat("_StartDelay") != startDelay){
-            renderer.sharedMaterial.SetFloat("_StartDelay", startDelay);
+        if (tempMaterial.GetFloat("_StartDelay") != startDelay){
+            tempMaterial.SetFloat("_StartDelay", startDelay);
         }
-        if (renderer.sharedMaterial.GetInt("_Shape") != (int)shape){
-            renderer.sharedMaterial.SetInt("_Shape", (int)shape);
+        if (tempMaterial.GetInt("_Shape") != (int)shape){
+            tempMaterial.SetInt("_Shape", (int)shape);
         }
-        if (renderer.sharedMaterial.GetFloat("_ConeAngle") != cone.angle){
-            renderer.sharedMaterial.SetFloat("_ConeAngle", cone.angle);
+        if (tempMaterial.GetFloat("_ConeAngle") != cone.angle){
+            tempMaterial.SetFloat("_ConeAngle", cone.angle);
         }
-        if (renderer.sharedMaterial.GetVector("_StartColor") != (Vector4)startColor){
-            renderer.sharedMaterial.SetVector("_StartColor", startColor);
+        if (tempMaterial.GetVector("_StartColor") != (Vector4)startColor){
+            tempMaterial.SetVector("_StartColor", startColor);
         }
-        if (renderer.sharedMaterial.GetFloat("_GravityModifier") != gravityModifier){
-            renderer.sharedMaterial.SetFloat("_GravityModifier", gravityModifier);
+        if (tempMaterial.GetFloat("_GravityModifier") != gravityModifier){
+            tempMaterial.SetFloat("_GravityModifier", gravityModifier);
         }
         
         for (int i = 0; i < MAX_COLLISION_PLANES; i++)
@@ -168,13 +179,15 @@ public class ParticleCircle : MonoBehaviour
                 if (plane_normal4.z < 0.000001f && plane_normal4.z > -0.000001f) plane_normal4.z = 0;
             }
             
-            if (renderer.sharedMaterial.GetVector("_CollisionPlaneCenter"+i) != plane_center4){
-                renderer.sharedMaterial.SetVector("_CollisionPlaneCenter"+i, plane_center4);
+            if (tempMaterial.GetVector("_CollisionPlaneCenter"+i) != plane_center4){
+                tempMaterial.SetVector("_CollisionPlaneCenter"+i, plane_center4);
             }
-            if (renderer.sharedMaterial.GetVector("_CollisionPlaneNormal"+i) != plane_normal4){
-                renderer.sharedMaterial.SetVector("_CollisionPlaneNormal"+i, plane_normal4);
+            if (tempMaterial.GetVector("_CollisionPlaneNormal"+i) != plane_normal4){
+                tempMaterial.SetVector("_CollisionPlaneNormal"+i, plane_normal4);
             }
         }
+
+        renderer.sharedMaterial = tempMaterial;
 
         int max_p = (int)Mathf.Ceil(startLifetime*emission.rateOverTime);
         if (max_p > maxParticles){
@@ -182,11 +195,10 @@ public class ParticleCircle : MonoBehaviour
         }
 
         Mesh mesh = GetComponent<MeshFilter>().sharedMesh;
-        if (max_p != (mesh.vertexCount*4)){
+        if (max_p != (mesh.vertexCount/4)){
             allocateParticles(max_p);
         }
 #endif
-        //@TODO: KILL EMITTER
     }
     
     void OnValidate()
