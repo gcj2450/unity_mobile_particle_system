c71974b2fde02f6ca7307e99fb13b77fa7fcd55d
Luiz0tavio <me@luizotav.io>
2018-11-11 14:37:35 -0200

    release version - export_package.sh update warning

--- export_package.sh
+++ export_package.sh
@@ -5,5 +5,11 @@ EXPORT_PATH=$PROJECT_PATH/Assets
 EXPORT_FILE=ParticleMobile.unitypackage
 
 PACK_PATH=Assets/ParticleMobile
-     
-/opt/Unity/Editor/Unity -batchmode -projectPath "$PROJECT_PATH" -exportPackage "$PACK_PATH" "$EXPORT_PATH/$EXPORT_FILE" -quit
+
+read -p "Must kill Unity first. Continue? [y/n] " -n 1 -r
+echo # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	pkill -9 Unity  
+	/opt/Unity/Editor/Unity -batchmode -projectPath "$PROJECT_PATH" -exportPackage "$PACK_PATH" "$EXPORT_PATH/$EXPORT_FILE" -quit
+fi
